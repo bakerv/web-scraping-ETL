3a762eb16aa93f9142e32d3e90a6513efb743a29
bakerv <80018640+bakerv@users.noreply.github.com>
2021-06-15 11:40:39 -0500

    html format improved for nasa mars exploration news

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -8,16 +8,19 @@ app = Flask(__name__)
 conn = 'mongodb://localhost:27017/'
 client= pymongo.MongoClient(conn)
 scrape_collection = client.mission_to_mars.scrape
+nasamars_collection = client.mission_to_mars.nasamars
+jplimages_collection = client.mission_to_mars.jplimages
+spacefacts_collection = client.mission_to_mars.spacefacts
+usgsimages_collection = client.mission_to_mars.usgsimages
 
 @app.route("/")
 def index():
-    scrape_collection = client.mission_to_mars.scrape
-    return render_template("index.html", list = scrape_collection.find())
+    return render_template("index.html", nasanews = nasamars_collection.find(), usgsimages = usgsimages_collection.find())
 
 @app.route("/scrape")
 def scrape():
     scrape_mars.scrape(scrape_collection)
-    return([x for x in scrape_collection.find()])
+    return([x for x in scrape_collection.find('Nasa_Mars_Exploration_Aticles')])
 
 
 if __name__ == "__main__":
