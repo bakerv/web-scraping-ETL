edf4f42ef73f275a35601ed2af5963e79c34b5f8
bakerv <80018640+bakerv@users.noreply.github.com>
2021-06-15 10:47:42 -0500

    mongdb successfully linked to the html. html format needs to be greatly improved

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,13 +1,24 @@
-from flask import Flask
-from flask_pymongo import PyMongo
-import scrape_mars.py
+from flask import Flask, render_template
+import pymongo
+import scrape_mars
+from pprint import pprint
 
 app = Flask(__name__)
-app.config("MONGO_URI") = conn
-mongo = PyMongo(app)
+
+conn = 'mongodb://localhost:27017/'
+client= pymongo.MongoClient(conn)
+scrape_collection = client.mission_to_mars.scrape
 
 @app.route("/")
+def index():
+    scrape_collection = client.mission_to_mars.scrape
+    return render_template("index.html", list = scrape_collection.find())
 
 @app.route("/scrape")
+def scrape():
+    scrape_mars.scrape(scrape_collection)
+    return([x for x in scrape_collection.find()])
 
-    
+
+if __name__ == "__main__":
+    app.run(debug=True)
